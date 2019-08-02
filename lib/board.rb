b1440b3aac5fc7ae06680122f4788b5ff24e4732
Robert Dunbar <robert_dunbar@me.com>
2019-08-02 20:20:56 +0100

    completed board and early player turns

--- lib/board.rb
+++ lib/board.rb
@@ -1,60 +1,123 @@
 class Board
 
-    attr_accessor :cells, :columns_full
+    attr_accessor :cells
+    attr_reader :pieces
 
     def initialize
-        @cells = Array.new(6) { Array.new(7) { 0 } }
-        @columns_full = []
+        initialize_pieces()
+        initialize_grid()
+        initialize_black()
+        initialize_white()
         show_board(cells)
     end
 
+    def initialize_pieces
+        @pieces = { white_king: "\u2654",
+                    white_queen: "\u2655",
+                    white_rook: "\u2656",
+                    white_bishop: "\u2657",
+                    white_knight: "\u2658",
+                    white_pawn: "\u2659",
+                    black_king: "\u265A",
+                    black_queen: "\u265B",
+                    black_rook: "\u265C",
+                    black_bishop: "\u265D",
+                    black_knight: "\u265E",
+                    black_pawn: "\u265F" }
+    end
+
+    def initialize_grid
+        @cells = Hash.new
+        ("a".."h").each do |letter|
+            (1..8).each do |num|
+                @cells[(letter + num.to_s).to_sym] = " "
+            end
+        end
+    end
+
+    def initialize_black
+        @cells[:a8], @cells[:h8] = @pieces[:black_rook], @pieces[:black_rook]
+        @cells[:b8], @cells[:g8] = @pieces[:black_knight], @pieces[:black_knight]
+        @cells[:c8], @cells[:f8] = @pieces[:black_bishop], @pieces[:black_bishop]
+        @cells[:d8] = @pieces[:black_queen]
+        @cells[:e8] = @pieces[:black_king]
+        ("a".."h").each do |col|
+            @cells[(col + "7").to_sym] = @pieces[:black_pawn]
+        end
+    end
+
+    def initialize_white
+        @cells[:a1], @cells[:h1] = @pieces[:white_rook], @pieces[:white_rook]
+        @cells[:b1], @cells[:g1] = @pieces[:white_knight], @pieces[:white_knight]
+        @cells[:c1], @cells[:f1] = @pieces[:white_bishop], @pieces[:white_bishop]
+        @cells[:d1] = @pieces[:white_queen]
+        @cells[:e1] = @pieces[:white_king]
+        ("a".."h").each do |col|
+            @cells[(col + "2").to_sym] = @pieces[:white_pawn]
+        end
+    end
+
     def show_board(cells)
-        for rows in 0..6
-            for columns in 0..6
-                print "\n\n" if columns == 0 and rows == 1
-                if rows == 0
-                    print "\t | #{columns + 1} |"
+        puts `clear`
+        9.downto(0) do |row|
+            print "\t"
+            (0..9).each do |col|
+                char = (col + 97 - 1).chr
+                sym = (char + row.to_s).to_sym
+                case row
+                when 0, 9
+                    if col != 0 && col != 9
+                        print "#{char} ".colorize(:color => :black, :background => :cyan)
+                    else
+                        print "  ".colorize(:color => :black, :background => :cyan)
+                    end
                 else
-                    print "\t | #{cells[rows-1][columns]} |" if cells[rows-1][columns] == 0
-                    print "\t | #{(cells[rows-1][columns]).blue} |" if cells[rows-1][columns] == "X"
-                    print "\t | #{(cells[rows-1][columns]).red} |" if cells[rows-1][columns] == "Y"
+                    if col == 0
+                        print "#{row} ".colorize(:color => :black, :background => :cyan)
+                    elsif col == 9
+                        print " #{row}".colorize(:color => :black, :background => :cyan)
+                    elsif (col + row) % 2 == 0
+                        print "#{@cells[sym]} ".colorize(:color => :black, :background => :blue)
+                    else
+                        print "#{@cells[sym]} ".colorize(:color => :black, :background => :white)
+                    end
                 end
             end
             puts ""
         end
-        puts "\n\n"
+        puts ""
     end
 
-    def new_moves_array(row, column)
-        new_moves = []
-        vertical_moves = []
-        horizonal_moves = []
-        incline_moves = []
-        decline_moves = []
-        for i in 1..7
-            vertical_moves << [row - (i - 4), column]
-            horizonal_moves << [row, column + (i - 4)]
-            incline_moves << [row - (i - 4),  column + (i - 4)]
-            decline_moves << [row + (i - 4),  column + (i - 4)]
-        end
-        new_moves << vertical_moves << horizonal_moves << incline_moves << decline_moves
-        new_moves.each do |line|
-            line.filter! { |pos| pos[0] <= 5 && pos[0] >= 0 && pos[1] <= 6 && pos[1] >= 0 }
-        end
-        new_moves
-    end
+    # def new_moves_array(row, column)
+    #     new_moves = []
+    #     vertical_moves = []
+    #     horizonal_moves = []
+    #     incline_moves = []
+    #     decline_moves = []
+    #     for i in 1..7
+    #         vertical_moves << [row - (i - 4), column]
+    #         horizonal_moves << [row, column + (i - 4)]
+    #         incline_moves << [row - (i - 4),  column + (i - 4)]
+    #         decline_moves << [row + (i - 4),  column + (i - 4)]
+    #     end
+    #     new_moves << vertical_moves << horizonal_moves << incline_moves << decline_moves
+    #     new_moves.each do |line|
+    #         line.filter! { |pos| pos[0] <= 5 && pos[0] >= 0 && pos[1] <= 6 && pos[1] >= 0 }
+    #     end
+    #     new_moves
+    # end
 
-    def check_win_lines(row, column)
-        new_moves = new_moves_array(row, column)
-        marker = cells[row][column]
-        result = false
-        new_moves.each do |line|
-            line.map! { |pos| cells[pos[0]][pos[1]] }
-        end
-        new_moves.each do |line|
-            line_summary = line.chunk { |pos| pos }.map { |char, num| [char, num.length]}
-            result = "winner" if line_summary.include?([marker, 4])
-        end
-        result
-    end
+    # def check_win_lines(row, column)
+    #     new_moves = new_moves_array(row, column)
+    #     marker = cells[row][column]
+    #     result = false
+    #     new_moves.each do |line|
+    #         line.map! { |pos| cells[pos[0]][pos[1]] }
+    #     end
+    #     new_moves.each do |line|
+    #         line_summary = line.chunk { |pos| pos }.map { |char, num| [char, num.length]}
+    #         result = "winner" if line_summary.include?([marker, 4])
+    #     end
+    #     result
+    # end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,19 @@
 require "./player.rb"
 require "./board.rb"
 
-require colorize
+require "colorize"
 
 class Game
     attr_reader :player_white, :player_black, :board
 
     def initialize
-        @player_white = Player.new(game_io("name"), :white)
-        @player_black = Player.new("Player 2 (red)", :black)
-        @board = Board.new()
-        @current_player = @player1
+        new_game = game_io("welcome")
+        if new_game == "2"
+            @player_white = Player.new(game_io("name", "Player 1 (white pieces) :"), :white)
+            @player_black = Player.new(game_io("name", "Player 2 (black pieces) :"), :black)
+            @board = Board.new()
+        end
+        @current_player = @player_white
     end
 
     def play
@@ -20,45 +23,78 @@ class Game
             break if result
             player_switch(@current_player)
         end
-        puts "Board is full! No winner" if result == "full"
-        puts "#{@current_player.name} is the winner!" if result == "winner"
+        # puts "Board is full! No winner" if result == "full"
+        # puts "#{@current_player.name} is the winner!" if result == "winner"
     end
 
     def player_turn(player)
-        column = ""
+        move_from = ""
         loop do
-            puts "Please select a unfilled column number to drop into?"
-            column = gets.chomp
-            break if check_valid_num?(column) && !@board.columns_full.include?(column)
+            puts "Please select a #{player.colour.to_s} piece to move :"
+            move_from = gets.chomp.downcase
+            p player.colour.to_s
+            break if check_valid_cell?(move_from) && get_piece_colour(move_from) == player.colour.to_s
         end
-        row = fill_board(player, column.to_i)
-        result = end_of_game?(row, column.to_i)
-        @board.show_board(@board.cells)
-        result
+        move_to = ""
+        loop do
+            puts "Please select a valid cell to move to. Available options : "
+            move_to = gets.chomp
+            break if check_valid_cell?(move_to)
+        end
+        # row = fill_board(player, column.to_i)
+        # result = end_of_game?(row, column.to_i)
+        # @board.show_board(@board.cells)
+        # result
     end
 
-    def fill_board(player, column)
-        for row in 5.downto(0)
-            if @board.cells[row][column - 1] == 0
-                @board.cells[row][column - 1] = "X" if player.colour == :blue
-                @board.cells[row][column - 1] = "Y" if player.colour == :red
-                @board.columns_full << column if row == 0
-                return row
+    def get_piece_colour(cell)
+        return false if @board.cells[cell.to_sym] == " "
+        return @board.pieces.key(@board.cells[cell.to_sym])[0..4]
+    end
+
+    # def fill_board(player, column)
+    #     for row in 5.downto(0)
+    #         if @board.cells[row][column - 1] == 0
+    #             @board.cells[row][column - 1] = "X" if player.colour == :blue
+    #             @board.cells[row][column - 1] = "Y" if player.colour == :red
+    #             @board.columns_full << column if row == 0
+    #             return row
+    #         end
+    #     end
+    # end
+
+    # def end_of_game?(row, column)
+    #     return "full" if ([1,2,3,4,5,6,7] - @board.columns_full).empty?
+    #     return @board.check_win_lines(row, column - 1)
+    # end
+
+    # def player_switch(player)
+    #     player == @player1 ? @current_player = @player2 : @current_player = @player1
+    # end
+
+    def check_valid_cell? (input)
+        return /[a-z][1-9]/.match (input)
+    end
+
+    def game_io(message, add_text="")
+        messages = {
+            "welcome" => "Welcome to command line chess!\n\n"\
+                         "Please enter 1 to load an existing game or 2 to play a new game :",
+            "name" => "Please enter the name of",
+            "from" => "Please select a cell"
+        }
+        puts `clear` if message == "welcome"
+        case message
+        when "welcome"
+            puts messages[message]
+            loop do
+                input = gets.chomp
+                return input if input == "1" || input == "2"
             end
+        when "name"
+            puts "#{messages[message]} #{add_text}"
+            return gets.chomp
         end
-    end
-
-    def end_of_game?(row, column)
-        return "full" if ([1,2,3,4,5,6,7] - @board.columns_full).empty?
-        return @board.check_win_lines(row, column - 1)
-    end
-
-    def player_switch(player)
-        player == @player1 ? @current_player = @player2 : @current_player = @player1
-    end
-
-    def check_valid_num? (input)
-        return /[1-9]/.match (input)
     end
 end
 
